65569b8f19e1530424c6b7a1e98b729d23283eed
Rajnish Kumar <110758110+kr-rajnish@users.noreply.github.com>
2023-11-02 00:46:45 +0530

    refactored code

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1 align="center">Minimalist Challnge</h1>
+
+<div align="center">
+  <h3>
+    <a href="https://minimalist-frontend-rajnish.netlify.app/">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://github.com/kr-rajnish/MinimaList-Assignment">
+      Solution
+    </a>
+    <span> | </span>
+  </h3>
+</div>
+
+<!-- OVERVIEW -->
+
+### Built With
+
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
+
+- [HTML]
+- [CSS]
+- [JavaScript]
+
+## Contact
+
+- GitHub [https://github.com/kr-rajnish ]
--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-// Get references to the modal and buttons
 var modal = document.getElementById("myModal");
 var openModalButton = document.querySelector("#viewButton");
 var closeModalButton = document.getElementById("closeModalButton");
 var chooseModalButton = document.getElementById("chooseModalButton");
 
-// Event listener to open the modal
 openModalButton.addEventListener("click", function () {
   modal.style.display = "block";
 });
 
-// Event listeners to close the modal
 closeModalButton.addEventListener("click", function () {
   modal.style.display = "none";
 });
 
 chooseModalButton.addEventListener("click", function () {
-  //   modal.style.display = "none";
   alert("Item selected");
 });
